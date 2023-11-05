1067567a28abc3e9626c0574d7c672f4247f8413
Malpercio <root@malpercio.dev>
2023-11-05 15:42:22 -0600

    add support for login

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,19 +1,24 @@
 "use client";
 
 import * as React from "react";
+import { SessionProvider } from "next-auth/react";
 import { NextUIProvider } from "@nextui-org/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import { Session } from "next-auth";
 
 export interface ProvidersProps {
   children: React.ReactNode;
   themeProps?: ThemeProviderProps;
+  session?: Session
 }
 
-export function Providers({ children, themeProps }: ProvidersProps) {
+export function Providers({ children, themeProps, session }: ProvidersProps) {
   return (
-    <NextUIProvider>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-    </NextUIProvider>
+    <SessionProvider session={session}>
+      <NextUIProvider>
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      </NextUIProvider>
+    </SessionProvider>
   );
 }
--- db/schema/index.ts
+++ db/schema/index.ts
@@ -1,22 +1,78 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { AdapterAccount } from "@auth/core/adapters";
+import {
+  integer,
+  primaryKey,
+  sqliteTable,
+  text,
+} from "drizzle-orm/sqlite-core";
 
 export const gear = sqliteTable("gear", {
-    id: text("id").primaryKey().default(''),
-    name: text("name").notNull(),
-    imagePath: text("imagePath").notNull(),
-    category: text("category").notNull(),
-    rarity: text("rarity").notNull(),
-})
-
-export const equippedGear = sqliteTable("equippedGear", {
-    id: text("id").primaryKey().default(''),
-    gear: text('gear', { mode: 'json' }).notNull()
+  id: text("id").primaryKey().default(""),
+  name: text("name").notNull(),
+  imagePath: text("imagePath").notNull(),
+  category: text("category").notNull(),
+  rarity: text("rarity").notNull(),
 });
 
-export const inventoryGear = sqliteTable('inventoryGear', {
-    id: text("id").primaryKey().default(''),
-    inventory: text('inventory', { mode: 'json' }).notNull()
-})
+export const equippedGear = sqliteTable("equippedGear", {
+  id: text("id").primaryKey().default(""),
+  gear: text("gear", { mode: "json" }).notNull(),
+});
+
+export const inventoryGear = sqliteTable("inventoryGear", {
+  id: text("id").primaryKey().default(""),
+  inventory: text("inventory", { mode: "json" }).notNull(),
+});
+
+export const users = sqliteTable("user", {
+  id: text("id").notNull().primaryKey(),
+  name: text("name"),
+  email: text("email").notNull(),
+  emailVerified: integer("emailVerified", { mode: "timestamp_ms" }),
+  image: text("image"),
+});
+
+export const accounts = sqliteTable(
+  "account",
+  {
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccount["type"]>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  (account) => ({
+    compoundKey: primaryKey(account.provider, account.providerAccountId),
+  })
+);
+
+export const sessions = sqliteTable("session", {
+  sessionToken: text("sessionToken").notNull().primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expires: integer("expires", { mode: "timestamp_ms" }).notNull(),
+});
+
+export const verificationTokens = sqliteTable(
+  "verificationToken",
+  {
+    identifier: text("identifier").notNull(),
+    token: text("token").notNull(),
+    expires: integer("expires", { mode: "timestamp_ms" }).notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey(vt.identifier, vt.token),
+  })
+);
 
 export type EquippedGear = typeof equippedGear.$inferSelect;
 export type InventoryGear = typeof inventoryGear.$inferSelect;
