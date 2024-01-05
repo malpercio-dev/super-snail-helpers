ba43e44c5c0134878c34b4e994b58577195b49cb
Malpercio <root@malpercio.dev>
2024-01-05 12:56:03 -0600

    allow for linking gear to a login

---
+++ types/next-auth.d.ts
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's id. */
+      id: string;
+
+      /** The user's name. */
+      name?: string;
+    };
+  }
+  interface Profile {
+    /** The user's id. */
+    id: string;
+
+    /** The user's name. */
+    name?: string;
+  }
+}
+
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    /** ID */
+    id?: string;
+  }
+}
