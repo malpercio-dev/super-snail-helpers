3a8873b14166b7ff5755bd20f56ec3e8e27acc54
Malpercio <root@malpercio.dev>
2024-02-27 11:08:24 -0600

    set inventory and save in db

---
+++ app/inventory/route.ts
@@ -0,0 +1,35 @@
+"use server";
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "../../db";
+import * as schema from "../../db/schema";
+import { uuidv7 } from "uuidv7";
+import { eq } from "drizzle-orm";
+
+export async function GET(request: NextRequest): Promise<NextResponse> {
+  const id = request.nextUrl.searchParams.get("iid")!;
+  const inventoryGear = await db
+    .select()
+    .from(schema.inventoryGear)
+    .where(eq(schema.inventoryGear.id, id)).limit(1);
+
+  return NextResponse.json(inventoryGear[0]);
+}
+
+export async function PUT(request: NextRequest): Promise<NextResponse> {
+  {
+    const inventoryGear = (await request.json()) as schema.InventoryGear;
+    if (!inventoryGear.id) inventoryGear.id = uuidv7();
+    const dbInventoryGear = await db
+      .insert(schema.inventoryGear)
+      .values(inventoryGear)
+      .onConflictDoUpdate({
+        target: schema.inventoryGear.id,
+        set: {
+          inventory: inventoryGear.inventory,
+        },
+      })
+      .returning();
+
+    return NextResponse.json(dbInventoryGear[0]);
+  }
+}
--- db/schema/index.ts
+++ db/schema/index.ts
@@ -5,4 +5,10 @@ export const equippedGear = sqliteTable("equippedGear", {
     gear: text('gear', { mode: 'json' }).notNull()
 });
 
+export const inventoryGear = sqliteTable('inventoryGear', {
+    id: text("id").primaryKey().default(''),
+    inventory: text('inventory', { mode: 'json' }).notNull()
+})
+
 export type EquippedGear = typeof equippedGear.$inferSelect;
+export type InventoryGear = typeof inventoryGear.$inferSelect;
