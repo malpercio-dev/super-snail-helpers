f764bb6fa4c33ae712a63462dce2e83f3525911c
Malpercio <root@malpercio.dev>
2024-02-27 11:08:23 -0600

    initial commit

--- config/site.ts
+++ config/site.ts
@@ -1,69 +1,24 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	name: "Malpercio's Super Snail Helpers",
+	description: "Let's snail together.",
 	navItems: [
 		{
 			label: "Home",
 			href: "/",
 		},
     {
-      label: "Docs",
-      href: "/docs",
+      label: "Gear",
+      href: "/gear",
     },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
-    }
 	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
-	],
+	navMenuItems: [],
 	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui.org",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
+		github: "https://github.com/malpercio-dev",
+		twitter: "https://twitter.com/SuperSnailUS",
+		docs: "https://supersnail.wiki.gg/wiki/Super_Snail_Wiki",
+		discord: "https://discord.gg/Hc4sRPsmmQ",
+        sponsor: "https://patreon.com/malpercio-dev"
 	},
 };
