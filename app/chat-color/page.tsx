18728465abdaff90e74535d83210851f6a1a9c9c
Malpercio <root@malpercio.dev>
2024-02-27 11:08:24 -0600

    rainbow text color; annoying bubble colors

--- app/chat-color/page.tsx
+++ app/chat-color/page.tsx
@@ -10,13 +10,16 @@ import {
 } from "@nextui-org/react";
 import { useState } from "react";
 
+import styles from "./styles.module.css";
+
 const CustomRadio = (props: RadioProps) => {
   const { children, ...otherProps } = props;
   return (
     <Radio
       {...otherProps}
       classNames={{
-        base: cn("rounded-lg", `bg-${otherProps.value ?? "white"}-900`, "m-1"),
+        base: cn("rounded-lg", "m-1", "bg-current"),
+        label: cn("text-black"),
       }}
     >
       {children}
@@ -26,28 +29,66 @@ const CustomRadio = (props: RadioProps) => {
 
 const colors = ["red", "orange", "yellow", "green", "cyan", "blue", "purple"];
 const colorTagRegex = new RegExp(`\\[(${colors.join("|")})\\]`, "g");
+const spacesOnlyRegex = new RegExp(/^\s*$/);
 const endTag = "[-]";
+const endSpan = "</span>";
+const customColorMap: { [key: string]: string } = {
+  "normal-bubble": "rgb(236,236,221)",
+  "whale-power-i": "rgb(220,233,234)",
+  "whale-power-ii": "rgb(243,231,196)",
+};
+
+function tagRainbow(string: string): string {
+  const splitter = string.includes(" ") ? " " : "";
+  let colorIndex = -1;
+  const tagged = string.split(splitter).map((segment) => {
+    if (spacesOnlyRegex.test(segment)) return segment;
+    colorIndex = (colorIndex + 1) % colors.length;
+    return colorTag(colors[colorIndex], segment);
+  });
+  return tagged.join(splitter);
+}
+
+function tagSpanRainbow(string: string): string {
+  const splitter = string.includes(" ") ? " " : "";
+  let colorIndex = -1;
+  const tagged = string.split(splitter).map((segment) => {
+    if (spacesOnlyRegex.test(segment)) return segment;
+    colorIndex = (colorIndex + 1) % colors.length;
+    return colorSpan(colors[colorIndex], segment);
+  });
+  return tagged.join(splitter);
+}
+
 function colorTag(color: string, string: string): string {
+  if (color === "rainbow") return tagRainbow(string);
+  if (Object.keys(customColorMap).includes(color))
+    return `[${customColorMap[color]}]${stripTags(string)}${endTag}`;
   return `[${color}]${stripTags(string)}${endTag}`;
 }
 
-function stripTags(string: string): string {
-  const newString = string.replaceAll(colorTagRegex, "").replaceAll(endTag, "");
-  return newString;
+function colorSpan(color: string, string: string): string {
+  if (color === "rainbow") return tagSpanRainbow(string);
+  if (Object.keys(customColorMap).includes(color))
+    return `<span class="${styles[`${color}-text`]}">${stripTags(
+      string
+    )}${endSpan}`;
+  return `<span class="${styles[`${color}-text`]}">${stripTags(
+    string
+  )}${endSpan}`;
 }
 
-function createPreview(string: string): string {
-  return string
-    .replaceAll(
-      colorTagRegex,
-      (match, color) => `<span class="text-${color}-900">`
-    )
-    .replaceAll(endTag, "</span>");
+function stripTags(string: string): string {
+  const newString = string
+    .replaceAll(colorTagRegex, "")
+    .replaceAll(endTag, "");
+  return newString;
 }
 
 export default function ChatColor() {
   const [text, setText] = useState("");
   const [color, setColor] = useState("red");
+  const [bubbleColor, setBubbleColor] = useState("normal-bubble");
   const [formattedText, setFormattedText] = useState("");
   const [previewText, setPreviewText] = useState({
     __html:
@@ -71,7 +112,7 @@ export default function ChatColor() {
     setText(value);
     const formattedText = colorTag(color, value);
     setFormattedText(formattedText);
-    const previewText = createPreview(formattedText);
+    const previewText = colorSpan(color, value);
     setPreviewText({
       __html: previewText,
     });
@@ -81,10 +122,14 @@ export default function ChatColor() {
     setColor(value);
     const formattedText = colorTag(value, text);
     setFormattedText(formattedText);
-    const previewText = createPreview(formattedText);
+    const previewText = colorSpan(value, text);
     setPreviewText({
       __html: previewText,
     });
+  };
+
+  const handleBubbleColorUpdate = (value: string) => {
+    setBubbleColor(value);
   };
 
   return (
@@ -101,41 +146,96 @@ export default function ChatColor() {
         value={text}
         onValueChange={handleTextUpdate}
       ></Textarea>
+      <h2>Text Color</h2>
       <RadioGroup
         orientation="horizontal"
         value={color}
         onValueChange={handleColorUpdate}
       >
-        <CustomRadio className="bg-red-900" value="red">
+        <CustomRadio className={styles["red-text"]} value="red">
           Red
         </CustomRadio>
-        <CustomRadio className="bg-orange-900" value="orange">
+        <CustomRadio className={styles["orange-text"]} value="orange">
           Orange
         </CustomRadio>
-        <CustomRadio className="bg-yellow-900" value="yellow">
+        <CustomRadio className={styles["yellow-text"]} value="yellow">
           Yellow
         </CustomRadio>
-        <CustomRadio className="bg-green-900" value="green">
+        <CustomRadio className={styles["green-text"]} value="green">
           Green
         </CustomRadio>
-        <CustomRadio className="bg-cyan-900" value="cyan">
+        <CustomRadio className={styles["cyan-text"]} value="cyan">
           Cyan
         </CustomRadio>
-        <CustomRadio className="bg-blue-900" value="blue">
+        <CustomRadio className={styles["blue-text"]} value="blue">
           Blue
         </CustomRadio>
-        <CustomRadio className="bg-purple-900" value="purple">
+        <CustomRadio className={styles["purple-text"]} value="purple">
           Purple
+        </CustomRadio>
+        <CustomRadio className={styles["normal-bubble-text"]} value="rainbow">
+          Rainbow
+        </CustomRadio>
+        <CustomRadio
+          className={styles["normal-bubble-text"]}
+          value="normal-bubble"
+        >
+          Normal Bubble
+        </CustomRadio>
+        <CustomRadio
+          className={styles["whale-power-i-text"]}
+          value="whale-power-i"
+        >
+          Whale Power I
+        </CustomRadio>
+        <CustomRadio
+          className={styles["whale-power-ii-text"]}
+          value="whale-power-ii"
+        >
+          Whale Power II
+        </CustomRadio>
+      </RadioGroup>
+      <h2>Chat bubble color</h2>
+      <RadioGroup
+        orientation="horizontal"
+        value={bubbleColor}
+        onValueChange={handleBubbleColorUpdate}
+      >
+        <CustomRadio
+          className={`${styles["normal-bubble"]} text-black`}
+          value="normal-bubble"
+        >
+          Normal Bubble
+        </CustomRadio>
+        <CustomRadio
+          className={`${styles["whale-power-i"]} text-black`}
+          value="whale-power-i"
+        >
+          Whale Power I
+        </CustomRadio>
+        <CustomRadio
+          className={`${styles["whale-power-ii"]} text-black`}
+          value="whale-power-ii"
+        >
+          Whale Power II
         </CustomRadio>
       </RadioGroup>
       <Textarea
         disabled
-        className="chatPreview"
         id="chatPreview"
         placeholder="Type your chat message here, then select any part of it and press a color button."
         value={formattedText}
       ></Textarea>
-      <div dangerouslySetInnerHTML={previewText}></div>
+      <div
+        className={cn(
+          styles[bubbleColor],
+          "rounded-lg",
+          "p-4",
+          "m-4",
+          "text-black"
+        )}
+        dangerouslySetInnerHTML={previewText}
+      ></div>
     </>
   );
 }
